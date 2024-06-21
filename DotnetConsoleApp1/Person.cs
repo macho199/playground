c33968277526529d94f3fe0301991b47770e1d2e
kjsdev <kjsdev@jobkorea.co.kr>
2024-06-21 14:56:48 +0900

    --- Auto Git Commit ---

--- DotnetConsoleApp1/Person.cs
+++ DotnetConsoleApp1/Person.cs
@@ -2,7 +2,7 @@ namespace DotnetConsoleApp1
 {
     class Person
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = String.Empty;
     }
 }
