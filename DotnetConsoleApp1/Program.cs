fa0befdc868ad077dd6aea9d65d960a1b0f7035c
kjsdev <kjsdev@jobkorea.co.kr>
2024-06-21 15:22:24 +0900

    --- Auto Git Commit ---

--- DotnetConsoleApp1/Program.cs
+++ DotnetConsoleApp1/Program.cs
@@ -10,3 +10,10 @@ Console.WriteLine(a[0]);
 var hong = new Person();
 hong.Id = 1;
 hong.Name = "honggildong";
+
+
+var jong = hong;
+jong.Id = 2;
+jong.Name = "jongseong";
+
+Console.WriteLine($"hong: {nameof(hong.Id)}: {hong.Id}, {nameof(hong.Name)}: {hong.Name}");
