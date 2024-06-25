d6ea03f441de0dcfcf905355636d55477202a093
kjsdev <kjsdev@jobkorea.co.kr>
2024-06-25 17:52:25 +0900

    --- Auto Git Commit ---

--- DotnetConsoleApp1/Program.cs
+++ DotnetConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Hello, World!");
 //string[] a = ["aaa", "bbb"]; // .net 8의 c# 12 에서 새로 작성하는 리터럴 방식
 string[] a = { "aaa", "bbb" }; // 전통 방식
 
-string[] b = new[] {11};
+string[] b = new[] { "cc" };
 
 Console.WriteLine(string.Join("|", a));
 
