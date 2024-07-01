23c9ba42cc89f8ed9f45343dd06f965d66bf008c
kjsdev <kjsdev@jobkorea.co.kr>
2024-07-01 10:08:55 +0900

    --- Auto Git Commit ---

--- DotnetConsoleApp1/Program.cs
+++ DotnetConsoleApp1/Program.cs
@@ -22,8 +22,17 @@ string[] ccc = {"kk", "uu"};
 Console.WriteLine(string.Join("|", ccc));
 
 string input = "~!@#$%^&*()_+-=`";
-string pattern = @"([\uD800-\uDBFF][\uDC00-\uDFFF])|([\u2600-\u26FF])|([\u2700-\u27BF])|([\u200D])|([\uFE0F])|([\p{Cs}\p{So}\p{Cn}\p{Co}\p{Sk}])";
-input = Regex.Replace(input, pattern, String.Empty);
+//string pattern = @"([\uD800-\uDBFF][\uDC00-\uDFFF])|([\u2600-\u26FF])|([\u2700-\u27BF])|([\u200D])|([\uFE0F])|([\p{Cs}\p{So}\p{Cn}\p{Co}\p{Sk}])";
+// 모든 이모지, 특수 문자, ZWJ 시퀀스를 제거하는 정규 표현식 패턴 (단, ^는 제외)
+//string pattern = @"([\uD800-\uDBFF][\uDC00-\uDFFF])|([\u2600-\u26FF])|([\u2700-\u27BF])|([\u200D])|([\uFE0F])|([\p{Cs}\p{So}\p{Cn}\p{Co}\p{Sk}]|[^\x00-\x7F&&[^\^]])";
+//input = Regex.Replace(input, pattern, String.Empty);
 
-Console.WriteLine(input);
+// 이모지 및 특수 문자 제거 패턴 (단, ^는 제외)
+string pattern = @"([\uD800-\uDBFF][\uDC00-\uDFFF])|([\u2600-\u26FF])|([\u2700-\u27BF])|([\u200D])|([\uFE0F])|([\p{Cs}\p{So}\p{Cn}\p{Co}\p{Sk}])";
+// 특수 문자 범위에서 ^를 제외한 문자 제거
+string specialPattern = @"[^\x00-\x7F^\^]";
+string result = Regex.Replace(input, pattern, String.Empty);
+result = Regex.Replace(result, specialPattern, String.Empty);
+
+Console.WriteLine(result);
 
